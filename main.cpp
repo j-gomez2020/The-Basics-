5d798ff426d34f00cbd23beeafee094c6b76db0a
Jasmin Gomez Heredia <32533734+j-gomez2020@users.noreply.github.com>
2019-09-22 20:23:52 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,65 @@
+//
+//  main.cpp
+//  Lab2_comsc165
+//  Total of 12 wrestlers on the team.
+//  Created by Jasmin Gomez Heredia  on 9/17/19.
+//  Copyright © 2019 Jasmin Gomez Heredia . All rights reserved.
+/*int main() {
+ const int NUM_VALS = 8;         // Number of elements in vector
+ vector<int> userVals(NUM_VALS); // User values
+ unsigned int i;                 // Loop index
+ 
+ cout << "Enter " << NUM_VALS << " integer values..." << endl;
+ for (i = 0; i < userVals.size(); ++i) {
+ cout << "Value: ";
+ cin >> userVals.at(i);
+ }
+ 
+ cout << "You entered: ";
+ for (i = 0; i < userVals.size(); ++i) {
+ cout << userVals.at(i) << " ";
+ }
+ cout << endl;
+ 
+ return 0;
+ }*/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    //All variable declarations are below.
+    double team = 12;
+    vector<double> weightls(team);
+    double avg_weight, max_weight;
+    double x;
+    
+    
+    // Use a loop to collecter userinput, only positive no. allowed.
+    double tot_weight;
+    cout <<"Enter weight for all members on your team : \n";
+    for (x = 0; x < weightls.size(); ++x) {
+         cin >> weightls.at(x);
+         tot_weight += weightls.at(x);// calc the sum of weight.
+    }
+    
+    //Try out fmax function from math libary to identify max valdouble fmax  (double x     , double y);
+    //algoritm for finding max
+    // Math logic for total and average computation
+    avg_weight = (tot_weight)/ 12;
+    max_weight = *max_element(weightls.begin(), weightls.end());
+    
+    // Displays the required outout for our user.
+    cout<< "These are the values entered: ";
+    for (x = 0; x < weightls.size(); ++x) {
+        cout << weightls.at(x)<< "   ";
+    }
+    cout<< "            \n"; 
+    cout<< "Total Weight : "<< tot_weight<< " \n";
+    cout<< "Average Weight : "<< avg_weight<< " \n";
+    cout<< "Max Weight : "<<max_weight<< " \n";
+    return 0;
+}
