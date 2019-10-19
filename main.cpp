0784844fa2defa3b4c52716853e27012e76d8ba6
Jasmin Gomez Heredia <32533734+j-gomez2020@users.noreply.github.com>
2019-10-18 20:09:24 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,46 +1,124 @@
 //
 //  main.cpp
-//  Lab2_comsc165
-//  Total of 12 wrestlers on the team.
-//  Created by Jasmin Gomez Heredia  on 9/17/19.
+//  classlab_gomez
+//  Created by Jasmin Gomez Heredia  on 10/17/19.
 //  Copyright © 2019 Jasmin Gomez Heredia . All rights reserved.
+//
 
 #include <iostream>
-#include <vector>
-#include <cmath>
-#include <algorithm>
+#include <string>
+#include <math.h>
 using namespace std;
+void testthisclass();
+void ogclassvals();
+
+//class declarator
+class Invoice {
+public:
+    //'setter' decalarations
+    void setpart (string);
+    void setdescrip(string);
+    void setquantity(int);
+    void setpriceperitem(int);
+    
+    //'getter' declarations
+    string getpart ();
+    string getdescrip();
+    int getquantity();
+    int getpriceperitem();
+    int getInvoiceAmount(int, int);
+private:
+    // var declarations
+    string part;
+    string description;
+    int quantity;
+    int priceperitem;
+    int value;
+};
+
+//setters
+void Invoice::setpart(string partno){
+    part = partno;
+}
+
+void Invoice:: setdescrip(string desc){
+    description = desc;
+}
+
+void Invoice::setquantity(int quan){
+    quantity = quan;
+}
+
+void Invoice:: setpriceperitem(int pricit){
+    priceperitem = pricit;
+    
+}
+
+/*-----------------------------------------------------------------------------*/
+//getters
+
+string Invoice::getpart(){
+    return part;
+}
+
+string Invoice:: getdescrip(){
+    return description;
+}
+
+int Invoice:: getquantity(){
+    return quantity;
+}
+
+int Invoice::getpriceperitem(){
+    return priceperitem;
+}
+
+int Invoice:: getInvoiceAmount(int quantity, int priceperitem){
+    value = fabs(quantity * priceperitem);
+    return value;
+}
+/*----------Test Run---------------------------------------------------*/
+
+void testthisclass(){
+    Invoice mytest;
+    int quant = 2 , price = -7;
+    mytest.setpart("12234");
+    mytest.setdescrip("Hammer");
+    mytest.setquantity(quant);
+    mytest.setpriceperitem(price);
+    
+    cout <<"Part Number: "<< mytest.getpart() << " \n";
+    cout <<"Part Description: "<< mytest.getdescrip() <<"  \n";
+    cout <<"Quantity: "<< mytest.getquantity()<< " \n";
+    cout <<"Price per item: $ "<< mytest.getpriceperitem()<< "  \n";
+    cout <<"Invoice amount: $ "<< mytest.getInvoiceAmount(quant, price)<< "  \n";
+}
+
+void ogclassvals(){
+    //assigns values that will be sent to setter function
+    Invoice myitem;
+    int quant = 3, price = 3;
+    myitem.setpart("11234");
+    myitem.setdescrip("screwdriver");
+    myitem.setquantity(quant);
+    myitem.setpriceperitem(price);
+    
+    
+    //print statements
+    cout <<"Part Number: "<< myitem.getpart() << " \n";
+    cout <<"Part Description: "<< myitem.getdescrip() <<"  \n";
+    cout <<"Quantity: "<< myitem.getquantity()<< " \n";
+    cout <<"Price per item: $ "<< myitem.getpriceperitem()<< "  \n";
+    cout <<"Invoice amount: $ "<< myitem.getInvoiceAmount(quant, price)<< "  \n";
+    cout <<"                                 \n";
+}
+/*------main function---------------------------------*/
 
 int main() {
-    //All variable declarations are below.
-    double team = 12;
-    vector<double> weightls(team);
-    double avg_weight, max_weight;
-    double x;
+    ogclassvals();// original values
+    cout <<"                          \n";
+    testthisclass(); // test function
+    cout<<"                           \n";
     
-    
-    // Use a loop to collecter userinput, only positive no. allowed.
-    double tot_weight;
-    cout <<"Enter weight for all members on your team : \n";
-    for (x = 0; x < weightls.size(); ++x) {
-         cin >> weightls.at(x);
-         tot_weight += weightls.at(x);// calc the sum of weight.
-    }
-    
-    //Try out fmax function from math libary to identify max valdouble fmax  (double x     , double y);
-    //algoritm for finding max
-    // Math logic for total and average computation
-    avg_weight = (tot_weight)/ 12;
-    max_weight = *max_element(weightls.begin(), weightls.end());
-    
-    // Displays the required outout for our user.
-    cout<< "These are the values entered: ";
-    for (x = 0; x < weightls.size(); ++x) {
-        cout << weightls.at(x)<< "   ";
-    }
-    cout<< "            \n"; 
-    cout<< "Total Weight : "<< tot_weight<< " \n";
-    cout<< "Average Weight : "<< avg_weight<< " \n";
-    cout<< "Max Weight : "<<max_weight<< " \n";
     return 0;
 }
